882d96874b9af62fbdb35632d77175df3c3f14a9
shilpavar <133573261+shilpavar@users.noreply.github.com>
2026-08-09 16:08:28 -0500

    Time: 100 ms (58.31%), Space: 49 MB (82.36%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cs
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public bool HasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow= head;
+        while(fast != null && fast.next != null   )
+        {
+            fast= fast.next.next;
+            slow= slow.next;
+            if (fast == slow)
+            {
+                return true;
+            }
+        }
+        return false;
+
+        
+        
+    }
+}
